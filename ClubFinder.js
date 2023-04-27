a419ce41bcbaab9c09d98c48b40ac9e65c901222
AliAngelinaaa <nanzum1140@gmail.com>
2023-04-27 10:56:06 -0400

    filter options added

--- ClubFinder.js
+++ ClubFinder.js
@@ -13,3 +13,19 @@ searchInput.addEventListener('keyup', function () {
         }
     });
 });
+
+const filterSelect = document.getElementById('filter-select');
+filterSelect.addEventListener('change', filterItems);
+function filterItems() {
+    const filterValue = document.getElementById('filter-select').value;
+    const items = document.querySelectorAll('.club-item');
+    
+    items.forEach(item => {
+      const category = item.getAttribute('data-category');
+      if (filterValue === 'all' || category === filterValue) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
